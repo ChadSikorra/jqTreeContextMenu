220b1ec669c8f5493f779f4e3b7511e37120c29f
Chad Sikorra <Chad.Sikorra@gmail.com>
2016-07-24 14:04:28 -0500

    Allow choosing the menu to display. Remove enable/disable functions.

--- jqTreeContextMenu.js
+++ jqTreeContextMenu.js
@@ -7,7 +7,8 @@
         var defaults = {
             menuFadeDuration: 250,
             selectClickedNode: true,
-            onContextMenuItem: null
+            onContextMenuItem: null,
+            contextMenuDecider: null
         };
         var settings = $.extend({}, defaults, options);
         var $el = this;
@@ -32,18 +33,20 @@
             this.bind('cm-jqtree.item.click', settings.onContextMenuItem);
         }
 
-        // This hash holds all menu items that should be disabled for a specific node.
-        var nodeToDisabledMenuItems = {};
-
         // Handle the contextmenu event sent from jqTree when user clicks right mouse button.
         $el.bind('tree.contextmenu', function (event) {
+            var menu = $menuEl;
+            if (typeof(settings.contextMenuDecider) == "function") {
+                var menuChoice = settings.contextMenuDecider(event.node);
+                menu = (typeof menuChoice == "string") ? $(menuChoice) : $menuEl;
+            }
             var x = event.click_event.pageX;
             var y = event.click_event.pageY;
             var yPadding = 5;
             var xPadding = 5;
 
-            var menuHeight = $menuEl.height();
-            var menuWidth = $menuEl.width();
+            var menuHeight = menu.height();
+            var menuWidth = menu.width();
             var windowHeight = $(window).height();
             var windowWidth = $(window).width();
 
@@ -56,13 +59,13 @@
             }
 
             // Must call show before we set the offset (offset can not be set on display: none elements).
-            $menuEl.fadeIn(settings.menuFadeDuration);
-            $menuEl.offset({ left: x, top: y });
+            menu.fadeIn(settings.menuFadeDuration);
+            menu.offset({ left: x, top: y });
 
             var dismissContextMenu = function () {
                 $(document).unbind('click.jqtreecontextmenu');
                 $el.unbind('tree.click.jqtreecontextmenu');
-                $menuEl.fadeOut(settings.menuFadeDuration);
+                menu.fadeOut(settings.menuFadeDuration);
             };
 
             // Make it possible to dismiss context menu by clicking somewhere in the document.
@@ -82,86 +85,13 @@
             }
 
             // Handle click on menu items, if it's not disabled.
-            $menuEl.find('li').off('click.contextmenu').on('click.contextmenu', function (e) {
+            menu.find('li').off('click.contextmenu').on('click.contextmenu', function (e) {
                 e.stopImmediatePropagation();
                 dismissContextMenu();
                 $el.trigger('cm-jqtree.item.click', [event.node, $(this)]);
             });
         });
 
-        this.disable = function () {
-            if (arguments.length === 0) {
-                // Called as: api.disable()
-                $menuEl.find('li:not(.disabled)').addClass('disabled');
-                $menuEl.find('li a').unbind('click');
-                nodeToDisabledMenuItems = {};
-            } else if (arguments.length === 1) {
-                // Called as: api.disable(['edit','remove'])
-                var items = arguments[0];
-                if (typeof items !== 'object') {
-                    return;
-                }
-                $menuEl.find('li > a').each(function () {
-                    var hrefValue = $(this).attr('href');
-                    var value = hrefValue.slice(hrefValue.indexOf("#") + 1, hrefValue.length);
-                    if ($.inArray(value, items) > -1) {
-                        $(this).closest('li').addClass('disabled');
-                        $(this).unbind('click');
-                    }
-                });
-                nodeToDisabledMenuItems = {};
-            } else if (arguments.length === 2) {
-                // Called as: api.disable(nodeName, ['edit','remove'])
-                nodeToDisabledMenuItems[arguments[0]] = arguments[1];
-            }
-        };
-
-        this.enable = function () {
-            if (arguments.length === 0) {
-                // Called as: api.enable()
-                $menuEl.find('li.disabled').removeClass('disabled');
-                nodeToDisabledMenuItems = {};
-            } else if (arguments.length === 1) {
-                // Called as: api.enable(['edit','remove'])
-                var items = arguments[0];
-                if (typeof items !== 'object') {
-                    return;
-                }
-
-                $menuEl.find('li > a').each(function () {
-                    var hrefValue = $(this).attr('href');
-                    var value = hrefValue.slice(hrefValue.indexOf("#") + 1, hrefValue.length)
-                    if ($.inArray(value, items) > -1) {
-                        $(this).closest('li').removeClass('disabled');
-                    }
-                });
-
-                nodeToDisabledMenuItems = {};
-            } else if (arguments.length === 2) {
-                // Called as: api.enable(nodeName, ['edit','remove'])
-                var nodeName = arguments[0];
-                var items = arguments[1];
-                if (items.length === 0) {
-                    delete nodeToDisabledMenuItems[nodeName];
-                } else {
-                    var disabledItems = nodeToDisabledMenuItems[nodeName];
-                    for (var i = 0; i < items.length; i++) {
-                        var idx = disabledItems.indexOf(items[i]);
-                        if (idx > -1) {
-                            disabledItems.splice(idx, 1);
-                        }
-                    }
-                    if (disabledItems.length === 0) {
-                        delete nodeToDisabledMenuItems[nodeName];
-                    } else {
-                        nodeToDisabledMenuItems[nodeName] = disabledItems;
-                    }
-                }
-                if (Object.keys(nodeToDisabledMenuItems).length === 0) {
-                    $menuEl.find('li.disabled').removeClass('disabled');
-                }
-            }
-        };
         return this;
     };
 } (jQuery));
